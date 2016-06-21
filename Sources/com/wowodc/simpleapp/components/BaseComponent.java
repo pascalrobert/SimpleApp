e182806c140d30ab3c5744ddbbf9dde58d1fb084
Pascal Robert <probert@macti.ca>
2016-06-21 19:11:47 -0400

    Very stupid app, just for building

---
+++ Sources/com/wowodc/simpleapp/components/BaseComponent.java
@@ -0,0 +1,24 @@
+package com.wowodc.simpleapp.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.components.ERXComponent;
+
+import com.wowodc.simpleapp.Application;
+import com.wowodc.simpleapp.Session;
+
+public class BaseComponent extends ERXComponent {
+	public BaseComponent(WOContext context) {
+		super(context);
+	}
+	
+	@Override
+	public Application application() {
+		return (Application)super.application();
+	}
+	
+	@Override
+	public Session session() {
+		return (Session)super.session();
+	}
+}
